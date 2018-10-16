69b7c2b8d426f821d5bb0b71677ada70836262ea
mc0re <mnikitin@hotmail.com>
2018-10-16 23:00:44 +0200

    Escape $.

--- SetupPi.sh
+++ SetupPi.sh
@@ -5,8 +5,8 @@ dpkg-reconfigure --frontend noninteractive locales
 cat >>~/.bashrc <<EOF
 export LS_OPTIONS='--color=auto'
 eval "\`dircolors\`"
-alias ls='ls $LS_OPTIONS'
-alias ll='ls $LS_OPTIONS -l'
-alias l='ls $LS_OPTIONS -lA'
+alias ls='ls \$LS_OPTIONS'
+alias ll='ls \$LS_OPTIONS -l'
+alias l='ls \$LS_OPTIONS -lA'
 export LANG=en_US.UTF-8
 EOF
