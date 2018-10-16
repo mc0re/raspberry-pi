6b727f0e55780693530994edad38f07c71f2a352
mc0re <mnikitin@hotmail.com>
2018-10-16 23:12:18 +0200

    Escape $, variant 3.

--- SetupPi.sh
+++ SetupPi.sh
@@ -2,11 +2,11 @@ update-locale "LANG=en_US.UTF-8"
 locale-gen --purge "en_US.UTF-8"
 dpkg-reconfigure --frontend noninteractive locales
 
-cat >>~/.bashrc <<EOF
+cat >>~/.bashrc << \EOF
 export LS_OPTIONS='--color=auto'
 eval "\`dircolors\`"
-alias ls='ls $$LS_OPTIONS'
-alias ll='ls $$LS_OPTIONS -l'
-alias l='ls $$LS_OPTIONS -lA'
+alias ls='ls $LS_OPTIONS'
+alias ll='ls $LS_OPTIONS -l'
+alias l='ls $LS_OPTIONS -lA'
 export LANG=en_US.UTF-8
 EOF
