6ca3d9bd698c8221c1346ed1fdfdb8aafd3f29c4
mc0re <mnikitin@hotmail.com>
2018-10-16 22:57:09 +0200

    Adding setup script.

---
+++ SetupPi.sh
@@ -0,0 +1,12 @@
+update-locale "LANG=en_US.UTF-8"
+locale-gen --purge "en_US.UTF-8"
+dpkg-reconfigure --frontend noninteractive locales
+
+cat >>~/.bashrc <<EOF
+export LS_OPTIONS='--color=auto'
+eval "\`dircolors\`"
+alias ls='ls $LS_OPTIONS'
+alias ll='ls $LS_OPTIONS -l'
+alias l='ls $LS_OPTIONS -lA'
+export LANG=en_US.UTF-8
+EOF
