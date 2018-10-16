bf402b37025161884e0d8abf15f599041eddb069
mc0re <mnikitin@hotmail.com>
2018-10-16 23:13:25 +0200

    Unescape backquotes.

--- SetupPi.sh
+++ SetupPi.sh
@@ -4,7 +4,7 @@ dpkg-reconfigure --frontend noninteractive locales
 
 cat >>~/.bashrc << \EOF
 export LS_OPTIONS='--color=auto'
-eval "\`dircolors\`"
+eval "`dircolors`"
 alias ls='ls $LS_OPTIONS'
 alias ll='ls $LS_OPTIONS -l'
 alias l='ls $LS_OPTIONS -lA'
